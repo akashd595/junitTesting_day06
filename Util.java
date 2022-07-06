1df84fc40b4e45e2d5303c49f084a517cb556d24
Akash Das <akashd595@gmail.com>
2022-07-06 18:02:01 +0530

    ADDED: TestDemo for day of week

--- Util.java
+++ Util.java
@@ -11,5 +11,13 @@ public class Util {
         temperatureInFahrenheit = ((temperatureInCelsius * 9) / 5) + 32;
         return temperatureInFahrenheit;
     }
+    public int  calculateDayOfWeek(int day, int month, int year) {
+        int y1, x, m, d1;
+        y1 = year - (14 - month) / 12;
+        x = y1 + (y1 / 4) - (y1 / 100) + (y1 / 400);
+        m = month + 12 * ((14 - month) / 12) - 2;
+        d1 = (day + x + 31 * m / 12) % 7;
+        return d1;
+    }
 }
 
